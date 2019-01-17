4a9111b7fb82352b749edd241e9b664d00dd2d23
Your Name <you@example.com>
2019-01-17 19:41:01 +0800

    输入车辆位置坐标

--- main.c
+++ main.c
@@ -1,8 +1,63 @@
 #include <stdio.h>
+#include <math.h>
+struct point
+{
+    int x;
+    int y;
+};
 
-int main(){
+int main()
+{
+    int index=0;
+    struct point arr[100];
 
-    printf("hello world!");
+    printf("huanyingjinruzhuancheguanlixitong");
+    while (1)
+    {
+        printf("1---tianjiacheliangweizhi\n");
+        printf("2---shanchucheliangweizhi\n");
+        printf("3---xianshiquanbucheliangweizhi\n");
+        printf("4---fanhuiliwodeweizhizuijindecheliang\n");
+        printf("5---tuichu\n");
+
+        printf("qingxuanzexiangyinggongneng\n");
+        int code;
+        scanf("%d",&code);
+        if (code == 1)
+        {
+            printf("qingshurucheliangweizhide x zuobiaohe y zuobiao:\n");
+            int x;
+            int y;
+            scanf("%d",&x);
+            scanf("%d",&y);
+
+            arr[index].x = x;
+            arr[index].y = y;
+            index++;
+            printf("tianjiawancheng,dianjihuichejixu\n");
+            char xx;
+            scanf("%c",&xx);
+            scanf("%c",&xx);
+        }
+        if (code == 2)
+        {
+            
+        }
+        if (code == 3)
+        {
+            
+        }
+        if (code == 4)
+        {
+            
+        }
+        if (code == 5)
+        {
+             printf("yingyongyijingtuichu\n");
+            break;
+        }
+        
+    }
 
     return 0;
 
