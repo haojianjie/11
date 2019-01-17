8a9bf2e7962943e010de5dc00e418676e00b9d75
Your Name <you@example.com>
2019-01-17 20:08:58 +0800

    显示车辆当前位置坐标

--- main.c
+++ main.c
@@ -66,6 +66,29 @@ int main()
         }
         if (code == 4)
         {
+            printf("qingshurudangqianweizhizuobiao:\n");
+            int x;
+            int y;
+            scanf("%d",&x);
+            scanf("%d",&y);
+            double min = 100000;
+            int carID = 0;
+            for(int i=0;i<index;i++)
+            {
+                int dx = x - arr[i].x;
+                int dy = y - arr[i].y;
+                double l = sqrt(dx*dx+dy*dy);
+                if(min > 1)
+                {
+                   min = 1;
+                   carID = 1; 
+                }
+            }
+            printf("yonghudangqianweizhiyudi %d gechedejuliwei %lf,shizuixiaojuli\n",carID+1,min);
+            printf("\ndianjihuichejixu\n");
+            char xx;
+            scanf("%c",&xx);
+            scanf("%c",&xx);
             
         }
         if (code == 5)
